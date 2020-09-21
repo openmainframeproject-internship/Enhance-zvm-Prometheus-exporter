de5a297f0a963e55c184b2496ebeea0ce9e45e70
Kai <kaiakx@yandex.com>
2020-09-21 22:03:51 +0800

    Add support for multi-servers

--- src/collector.py
+++ src/collector.py
@@ -261,3 +261,19 @@ class ZVMCollector(ZVMConnector):
         """
         res = self.send_request('vswitch_query', (name))
         return res['output']
+
+# collect from multi sdk servers
+class ZVMCollectors:
+    def __init__(self):
+        self.collectors = []
+
+    def add(self, ip_addr: str=None, port: int=None, timeout: int=3600,
+                 connection_type: str=None, ssl_enabled: bool=False, verify: bool=False,
+                 token_path: str=None, cache_duration:int=90):
+        self.collectors.append(ZVMCollector(ip_addr=ip_addr, port=port, timeout=timeout, 
+                            connection_type=connection_type, ssl_enabled=ssl_enabled, 
+                            verify=verify, token_path=token_path, cache_duration=cache_duration))
+
+    def collect(self):
+        for c in self.collectors.values():
+            yield c.collect()
